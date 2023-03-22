41a6aeecb2665f2e596fdcc1a69cb30050cc7091
Victor Manuel Cabrera Abreu (ext) <victor.cabrera.external@atos.net>
2023-03-22 16:55:24 +0000

    #8,#9; Creacion del back y frontend

---
+++ source/frontend/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { IndexComponent } from './componentes/index/index.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { RegisterComponent } from './componentes/register/register.component';
+import { GamesComponent } from './componentes/games/games.component';
+import { UserComponent } from './componentes/user/user.component';
+import { PostsComponent } from './componentes/posts/posts.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    IndexComponent,
+    LoginComponent,
+    RegisterComponent,
+    GamesComponent,
+    UserComponent,
+    PostsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
