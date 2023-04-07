91c0fb64642194bdd1e7bb066234dc0d2f4e6237
vmcabreu <victormanueltoa@gmail.com>
2023-04-07 19:21:14 +0100

    Testeo de API REST

--- source/backend/usuario.php
+++ source/backend/usuario.php
@@ -1,11 +1,11 @@
 <?php 
-require_once __DIR__. "/inc/config.php";
+require_once "/inc/config.php";
 if ($_SERVER['REQUEST_METHOD'] == 'GET')
 {
     if (isset($_GET['id']))
     {
       //Mostrar un post
-      header("HTTP/1.1 200 OK");
+      header("Content-Type: application/json");
       echo json_encode(DAOUsuario::buscarUsuario(intval($_GET['id'])));
       exit();
 	  }
