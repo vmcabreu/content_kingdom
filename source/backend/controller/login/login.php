7d1fd62c278acb8bba09100643eca38ce2ed715e
vmcabreu <victormanueltoa@gmail.com>
2023-04-25 17:07:29 +0100

    #13; Login base

--- source/backend/controller/login/login.php
+++ source/backend/controller/login/login.php
@@ -1,7 +1,9 @@
 <?php
 require_once(__DIR__ . "../../../inc/bootstrap.php");
 header("Access-Control-Allow-Origin: *");
-header('Content-Type: application/json');
+header('Content-Type', 'application/json');
+header('Access-Control-Request-Headers', '*');
+header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_POST['usuario']) && isset($_POST['passwd'])) {
         $user = $_POST['usuario'];
