e1a4e4ee9277022868e2c5b3205c37d117857dea
vmcabreu <victormanueltoa@gmail.com>
2023-04-25 17:16:06 +0100

    #13; Login base

--- source/backend/controller/login/login.php
+++ source/backend/controller/login/login.php
@@ -1,11 +1,11 @@
 <?php
 require_once(__DIR__ . "../../../inc/bootstrap.php");
 header("Access-Control-Allow-Origin: *");
-header('Content-Type', 'application/json');
 header('Access-Control-Request-Headers', '*');
-header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_POST['usuario']) && isset($_POST['passwd'])) {
+        echo file_get_contents("php://input");
         $user = $_POST['usuario'];
         $passwd = $_POST['passwd'];
         $stmt = BaseDAO::consulta("SELECT * FROM usuarios WHERE usuario='$user' LIMIT 1");
@@ -13,6 +13,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
             if (password_verify($passwd, $usuario["passwd"])) {
                 $usertoken =  new Usuario($usuario['id'], $usuario['usuario'], $usuario['passwd'], $usuario['email']);
+                header('Content-Type: application/json');
                 echo $usertoken;
             } else {
                 header('HTTP/1.0 401 Unauthorized');
