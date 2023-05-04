43c45bd9ed46d848dda790bfcfc5513d40607f33
vmcabreu <victormanueltoa@gmail.com>
2023-05-04 03:56:49 +0100

    #13 Refactorización del login

--- source/backend/controller/login/loginv2.php
+++ source/backend/controller/login/loginv2.php
@@ -5,26 +5,26 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_POST['usuario']) && isset($_POST['passwd'])) {
         $user = $_POST['usuario'];
         $passwd = $_POST['passwd'];
-        if (DAOUsuario::validarLogin($passwd,$user)) {
-            $bdUser = DAOUsuario::loginGetUser($passwd,$user);
+        if (DAOUsuario::validarLogin($passwd, $user)) {
+            $bdUser = DAOUsuario::loginGetUser($passwd, $user);
             Token::loginJWT($bdUser);
             http_response_code(200);
-        }else{
+        } else {
             http_response_code(422);
         }
     }
 }
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    if (isset($_POST['usuario']) && isset($_POST['passwd'])) {
-        $user = $_POST['usuario'];
-        $passwd = $_POST['passwd'];
-        if (DAOUsuario::validarLogin($passwd,$user)) {
-            $bdUser = DAOUsuario::loginGetUser($passwd,$user);
-            header('Content-Type: application/json');
-            echo json_encode(array( 'token' => $bdUser->token));
-        }else{
-            http_response_code(422);
-        }
+    $jsonInput = file_get_contents('php://input');
+    $jsonObject = json_decode($jsonInput, true);
+    $user = $jsonObject->user;
+    $user = $jsonObject->passwd;
+    if (DAOUsuario::validarLogin($passwd, $user)) {
+        $bdUser = DAOUsuario::loginGetUser($passwd, $user);
+        header('Content-Type: application/json');
+        echo json_encode(array('token' => $bdUser->token));
+    } else {
+        http_response_code(422);
     }
 }
