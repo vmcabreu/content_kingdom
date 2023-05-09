ddf865ca0d71405e96d4e90e0d3bc01d5290fc1b
vmcabreu <victormanueltoa@gmail.com>
2023-05-09 04:56:52 +0100

    #27 Petición post de Publicaciones

--- source/backend/model/DAOPublicacion.php
+++ source/backend/model/DAOPublicacion.php
@@ -26,7 +26,8 @@ class DAOPublicacion
 
     public static function aniadirPublicacion(Publicacion $post): int
     {
-        $sql = "INSERT INTO publicaciones VALUES (null,'$post->id_usuario','$post->id_videojuego','$post->fecha','$post->megusta','$post->mensaje','$post->adjunto','$post->plataforma','$post->etiqueta'";
+        $newFecha = DateTime::createFromFormat('Y-m-d', $post->fecha)->format('Y-m-d');
+        $sql = "INSERT INTO publicaciones VALUES (null,'$post->id_usuario','$post->id_videojuego','$newFecha','$post->megusta','$post->mensaje','$post->adjunto','$post->plataforma','$post->etiqueta'";
         return BaseDAO::consulta($sql);
     }
 }
