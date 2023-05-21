8141bb88cc9e9be70a0008a2b41b0c136ef99263
vmcabreu <victormanueltoa@gmail.com>
2023-05-21 02:29:51 +0100

    #35 Creados endpoints para Comentarios

--- source/backend/controller/posts/post.php
+++ source/backend/controller/posts/post.php
@@ -5,7 +5,7 @@ header("Access-Control-Allow-Origin: *");
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     if (isset($_GET['listType']) && $_GET['listType'] == "likes") {
         $listMegusta = DAOPublicacion::getPublicacionByMegusta();
-        if ($listMegusta != null) {
+        if ($listMegusta) {
             echo json_encode($listMegusta, JSON_UNESCAPED_UNICODE);
         } else {
             http_response_code(404);
@@ -17,7 +17,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     if (isset($_GET['idUsuario'])) {
         $id = intval($_GET['idUsuario']);
         $listaFromUsuario = DAOPublicacion::getPublicacionByUsuario($id);
-        if ($listaFromUsuario != null) {
+        if ($listaFromUsuario) {
             echo json_encode($listaFromUsuario, JSON_UNESCAPED_UNICODE);
         } else {
             http_response_code(404);
@@ -29,7 +29,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     if (isset($_GET['idJuego'])) {
         $id = intval($_GET['idJuego']);
         $listaFromJuego = DAOPublicacion::getPublicacionByVideojuego($id);
-        if ($listaFromJuego != null) {
+        if ($listaFromJuego) {
             echo json_encode($listaFromJuego, JSON_UNESCAPED_UNICODE);
         } else {
             http_response_code(404);
@@ -39,7 +39,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     }
 
     $list = DAOPublicacion::listaPublicacion();
-    if ($list != null) {
+    if ($list) {
         echo json_encode($list, JSON_UNESCAPED_UNICODE);
     } else {
         http_response_code(404);
@@ -48,15 +48,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 } elseif ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $json = file_get_contents('php://input');
     $data = json_decode($json, true);
-    $publicacion = new Publicacion();
-    $publicacion->id_usuario = $data['id_usuario'];
-    $publicacion->id_videojuego = $data['id_videojuego'];
-    $publicacion->fecha = $data['fecha'];
-    $publicacion->megusta = $data['megusta'];
-    $publicacion->mensaje = $data['mensaje'];
-    $publicacion->adjunto = $data['adjunto'];
-    $publicacion->plataforma = $data['plataforma'];
-    $publicacion->etiqueta = $data['etiqueta'];
+    $publicacion = Publicacion::crearPublicacion($data);
     $response = DAOPublicacion::aniadirPublicacion($publicacion);
     http_response_code($response > 0 ? 200 : 422);
 } elseif ($_SERVER['REQUEST_METHOD'] == 'DELETE'){
@@ -67,3 +59,4 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
         return;
     }
 }
+?>
