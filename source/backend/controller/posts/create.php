660dc7b8f7956f5b2764b57bdc3feace25da4d4d
vmcabreu <victormanueltoa@gmail.com>
2023-05-23 22:47:29 +0100

    Fixing errors

--- source/backend/controller/posts/create.php
+++ source/backend/controller/posts/create.php
@@ -4,6 +4,7 @@ header("Access-Control-Allow-Origin: *");
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $json = file_get_contents('php://input');
     $array = json_decode($json, true);
+    var_dump($array);
     if (DAOPublicacion::aniadirPublicacion(Publicacion::crearPublicacion($array))) {
         http_response_code(200);
     } else {
